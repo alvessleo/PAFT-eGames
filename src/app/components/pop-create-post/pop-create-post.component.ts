3acf44edd294a09c9ad37ee761fd3f687aa6f24f
Leonardo Alves <alvesleo2002@gmail.com>
2023-02-19 16:22:30 -0300

    POP UP - Utilizando Material Angular Dialog para fazer pop ups + image preview

--- src/app/components/pop-create-post/pop-create-post.component.ts
+++ src/app/components/pop-create-post/pop-create-post.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-pop-create-post',
   templateUrl: './pop-create-post.component.html',
-  styleUrls: ['./pop-create-post.component.scss']
+  styleUrls: ['./pop-create-post.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PopCreatePostComponent {
+  url = '../../../assets/publication-image-mock.svg';
 
+  constructor(private dialogRef : MatDialog){
+
+  }
+
+  closeDialog(){
+    this.dialogRef.closeAll();
+  }
+
+  onSelectFile(e: any){
+    if(e.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload=(event: any) => {
+        this.url = event.target.result;
+      }
+    }
+  }
 }

23e43b09a812e62da832fc0bb56ef4a10a655858
Salgado2004 <leosalgado2004@gmail.com>
2023-02-21 18:40:58 -0300

    controle de session

--- src/app/pages/feed-content/feed-content.component.ts
+++ src/app/pages/feed-content/feed-content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-feed-content',
@@ -10,11 +11,14 @@ export class FeedContentComponent {
   sessionId: any;
   username!: string;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
   }
 
   ngOnInit(){
     this.sessionId = sessionStorage.getItem("sessionId");
+    if (this.sessionId == "unset"){
+      this.router.navigate(['/']);
+    }
     this.userService.usuarioDaSessao(this.sessionId).subscribe(usuario => {
       var jsonResult = JSON.parse(JSON.stringify(usuario))
       this.username = jsonResult['usuario']['username']
